f1ec7b08272fad55f075baafcf172e9005319f5d
satish <satishkonduru0225@gmail.com>
2024-03-06 13:20:57 +0530

    Working on ProductAPI

--- Backend/routes/productAPI.js
+++ Backend/routes/productAPI.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { Product } = require('../models/productModel')
+const {Category} = require('../models/categoryModel')
 const { authenticateToken } = require('../AuthServices/authorization')
 const { checkRole } = require('../AuthServices/checkRole')
+const mongoose = require('mongoose')
 
 //Getting all Products
 router.get('/getAllProducts', authenticateToken,  async(req, res) => {
@@ -24,11 +26,20 @@ router.get('/getAllProducts', authenticateToken,  async(req, res) => {
 //Adding New Product
 router.post('/addProduct', authenticateToken, checkRole, async (req, res) => {
     const data  = req.body
+    const categoryId = data.category
+    checkCategoryId = await Category.findById(categoryId)
+ 
+    if(checkCategoryId == '' || checkCategoryId == null){
+        return res.status(500).send({
+            message: 'Invalid Category'
+        })
+    }
     const product = new Product({
         name: data.name,
         description: data.description,
         price: data.price,
-        countInStock: data.countInStock
+        countInStock: data.countInStock,
+        category: data.category
         
     })
     newProduct = await product.save()
@@ -46,5 +57,50 @@ router.post('/addProduct', authenticateToken, checkRole, async (req, res) => {
 })
 
 
-//Get Product By Id
+//Get Product Details by Id along with its category details
+
+router.get('/getById/:id', authenticateToken, async (req, res) => {
+    const productId = req.params.id
+    productDetails = await Product.findById(productId).populate('category')
+    
+    if(!productDetails){
+       return res.status(500).send({
+                message: 'Product was not found'
+            })
+    }  
+    else{
+        return res.status(200).send({
+            message: 'Product Found.',
+            product: productDetails
+        })
+    }
+    
+   
+})
+
+//Update Product
+router.patch('/update/:id', authenticateToken, checkRole, async (req, res) => {
+    const productId= req.params.id
+    const newData = req.body
+    if(!mongoose.isValidObjectId(productId)){
+        return res.status(500).send({
+            message: 'Invalid Object Id'
+        })
+    }
+    updateProduct = await Product.findByIdAndUpdate(productId, {name: newData.name}, {new: true})
+    if(!updateProduct){
+       return res.status(500).send({
+            message: "Invalid Product Selection"
+        })
+    }
+    else{
+       return res.status(200).send({
+            message: 'Product Updated Successfully',
+            newData: updateProduct
+        })
+    }
+
+})
+
+//Delete Product
 module.exports = router

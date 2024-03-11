32ce518d8af60f986a709da166a2060b2bb87b86
satish <satishkonduru0225@gmail.com>
2024-03-11 17:06:36 +0530

    created components for Men, Women and Kids under admin level

--- Frontend/src/app/modules/admin/admin-routing.module.ts
+++ Frontend/src/app/modules/admin/admin-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MenComponent } from './products/men/men.component';
+import { WomenComponent } from './products/women/women.component';
+import { KidsComponent } from './products/kids/kids.component';
 
 const routes: Routes = [
   {
@@ -10,8 +13,23 @@ const routes: Routes = [
   },
   {
     path: 'dashboard',
-    component: DashboardComponent
-  }
+    component: DashboardComponent,
+    children: [
+      {
+        path:'products/men',
+        component: MenComponent
+      },
+      {
+        path:'products/women',
+        component: WomenComponent
+      },
+      {
+        path:'products/kids',
+        component: KidsComponent
+      }
+    ]
+  },
+  
 ];
 
 @NgModule({

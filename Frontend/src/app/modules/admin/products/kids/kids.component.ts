47e053c61d0cf602e9c3210002cff763e7529cfd
satish <satishkonduru0225@gmail.com>
2024-03-18 10:50:18 +0530

    Getting of Products for Kids done

--- Frontend/src/app/modules/admin/products/kids/kids.component.ts
+++ Frontend/src/app/modules/admin/products/kids/kids.component.ts
@@ -1,12 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { AngularMaterialModule } from '../../../angular-material/angular-material.module';
+import { CommonModule } from '@angular/common';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { Observable, Subject, debounceTime, distinctUntilChanged, map, shareReplay, startWith, switchMap } from 'rxjs';
+import { productModel } from '../../../../shared/models/model';
+import { KidsService } from './kidsService';
+import { LoaderService } from '../../../../services/loader.service';
+import { FormsModule } from '@angular/forms';
+export interface UserData {
+  name: string;
+  description: string;
+  price: number;
+  date: Date;
+  countInStock: number;
+  brand: string;
+}
+
 
 @Component({
   selector: 'app-kids',
   standalone: true,
-  imports: [],
+  imports: [AngularMaterialModule, CommonModule, FormsModule],
   templateUrl: './kids.component.html',
   styleUrl: './kids.component.css'
 })
-export class KidsComponent {
+export class KidsComponent implements OnInit{
+  dataSource = new MatTableDataSource<UserData>();
+  paginatedData: UserData[] = [];
+  itemsPerPage = 5; // Number of items per page
+  itemsPerPageOptions = [5, 10, 20]; // Options for items per page
+
+  displayedColumns: string[] = ['name', 'price', 'color', 'countInStock']
+  kidsProducts$! : Observable<productModel[]> 
+  kidsService = inject(KidsService)
+  loaderService = inject(LoaderService)
+  spinnerSize: number = 30;
+  searchKey : any;
+  private searchTerms = new Subject<string>();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+ 
+  constructor() { }
+  ngOnInit(){
+    this.kidsProducts$ = this.searchTerms.pipe(
+      startWith(''),
+      debounceTime(500), // Debounce time of 300ms
+      distinctUntilChanged(), // Only emit when the search term changes
+      switchMap((searchKey: string) => this.getProducts(searchKey))
+    ).pipe(shareReplay())
+  }
+
+
+  getProducts(searchKey: string): Observable<any> { // Return type explicitly set to Observable<any>
+    return this.kidsProducts$= this.loaderService.showLoader(this.kidsService.getProducts()).pipe(
+      map((res: any) => {
+        const productArray = res.products || [];
+        return productArray.filter(
+          (product: any) =>
+            product.category.name == "Kids" &&
+            (product.name
+              .trim()
+              .toLowerCase()
+              .includes(searchKey.trim().toLowerCase()) ||
+              product.color
+                .trim()
+                .toLowerCase()
+                .includes(searchKey.trim().toLowerCase()))
+        );
+      })
+    );
+  }
+
+  applyFilter(value: any) {
+    // this.getProducts(value);
+    this.searchTerms.next(value);
+  }
+  onSearchClear() {
+    this.searchKey = "";
+    this.applyFilter("");
+  }
 
 }
--- Frontend/src/app/modules/angular-material/angular-material.module.ts
+++ Frontend/src/app/modules/angular-material/angular-material.module.ts
@@ -23,6 +23,7 @@ import {MatTableModule} from '@angular/material/table';
 
 
 
+
 const materialComponents: any[] = [
   MatToolbarModule,
   MatIconModule,

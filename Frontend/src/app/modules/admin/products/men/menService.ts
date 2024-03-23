0c41c2f9ba9d064d51b7a3e7e868de063e399b97
satish <satishkonduru0225@gmail.com>
2024-03-23 09:12:05 +0530

    Work started on Men Products component

--- Frontend/src/app/modules/admin/products/men/menService.ts
+++ Frontend/src/app/modules/admin/products/men/menService.ts
@@ -12,7 +12,7 @@ export class MenService{
     private _url = environment.apiUrl;
     private _http = inject(HttpClient)
   
-
+ 
    
     getProducts(): Observable<productModel[]>{
         return this._http.get<productModel[]>(`${this._url}/product/getAllProducts`).pipe(
--- Frontend/src/app/pages/men-products/men-products.component.ts
+++ Frontend/src/app/pages/men-products/men-products.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { MenService } from '../../modules/admin/products/men/menService';
+import { Observable, map } from 'rxjs';
+import { productModel } from '../../shared/models/model';
+import { CommonModule } from '@angular/common';
+import { AngularMaterialModule } from '../../modules/angular-material/angular-material.module';
+import { LoaderService } from '../../services/loader.service';
 
 @Component({
   selector: 'app-men-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AngularMaterialModule],
   templateUrl: './men-products.component.html',
   styleUrl: './men-products.component.css'
 })
-export class MenProductsComponent {
+export class MenProductsComponent implements OnInit{
+  timestamp = Date.now();
+  menService = inject(MenService)
+  menProducts$!: Observable<productModel[]>;
+  loaderService = inject(LoaderService);
+  spinnerSize: number = 30;
+prodcut: any;
+  ngOnInit(): void {
+      this.getProducts()
+  }
+  getProducts() {
+    const products$ = this.menService.getProducts()
+    products$.subscribe(res => console.log(res))
+    const loadProducts$ = this.loaderService.showLoader(products$);
+    this.menProducts$ = loadProducts$.pipe(
+      map((res: any) => {
+        const productArray = res.products || [];
+        return productArray.filter(
+          (product: any) =>
+            product.category.name == "Men"
+          );
+      })
+    );
+  }
+  
 
 }

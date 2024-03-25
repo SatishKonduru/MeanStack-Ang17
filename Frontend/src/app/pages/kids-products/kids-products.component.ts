23168a9afd770b369c33a8a67c43224dcbe26ea9
satish <satishkonduru0225@gmail.com>
2024-03-25 10:17:44 +0530

    Displaying products for men, women and kids was completed

--- Frontend/src/app/pages/kids-products/kids-products.component.ts
+++ Frontend/src/app/pages/kids-products/kids-products.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { MenService } from '../../modules/admin/products/men/menService';
+import { productModel } from '../../shared/models/model';
+import { Observable, map } from 'rxjs';
+import { LoaderService } from '../../services/loader.service';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ProductDetailsComponent } from '../product-details/product-details.component';
+import { CommonModule } from '@angular/common';
+import { AngularMaterialModule } from '../../modules/angular-material/angular-material.module';
 
 @Component({
   selector: 'app-kids-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AngularMaterialModule],
   templateUrl: './kids-products.component.html',
   styleUrl: './kids-products.component.css'
 })
-export class KidsProductsComponent {
+export class KidsProductsComponent implements OnInit{
+  timestamp = Date.now();
+  menService = inject(MenService)
+  kidsProducts$!: Observable<productModel[]>;
+  loaderService = inject(LoaderService);
+  spinnerSize: number = 30;
+  dialog = inject(MatDialog)
+
+  ngOnInit(): void {
+    this.getProducts()
+}
+
+getProducts() {
+  const products$ = this.menService.getProducts()
+  const loadProducts$ = this.loaderService.showLoader(products$);
+  this.kidsProducts$ = loadProducts$.pipe(
+    map((res: any) => {
+      const productArray = res.products || [];
+      return productArray.filter(
+        (product: any) =>
+          product.category.name == "Kids"
+        );
+    })
+  );
+}
+
+openProductDetails(product: any){
+  console.log("Product Details: ", product)
+  const dialogConfig = new MatDialogConfig()
+  dialogConfig.data = product
+  dialogConfig.width = '1024px'
+  dialogConfig.autoFocus = true
+  dialogConfig.disableClose = true
+  this.dialog.open(ProductDetailsComponent, dialogConfig)
+}
+
 
 }
--- Frontend/src/app/pages/women-products/women-products.component.ts
+++ Frontend/src/app/pages/women-products/women-products.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { AngularMaterialModule } from '../../modules/angular-material/angular-material.module';
+import { MenService } from '../../modules/admin/products/men/menService';
+import { Observable, map } from 'rxjs';
+import { productModel } from '../../shared/models/model';
+import { LoaderService } from '../../services/loader.service';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ProductDetailsComponent } from '../product-details/product-details.component';
 
 @Component({
   selector: 'app-women-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AngularMaterialModule],
   templateUrl: './women-products.component.html',
   styleUrl: './women-products.component.css'
 })
-export class WomenProductsComponent {
+export class WomenProductsComponent implements OnInit{
+  timestamp = Date.now();
+  menService = inject(MenService)
+  womenProducts$!: Observable<productModel[]>;
+  loaderService = inject(LoaderService);
+  spinnerSize: number = 30;
+  dialog = inject(MatDialog)
+
+  ngOnInit(): void {
+    this.getProducts()
+}
+getProducts() {
+  const products$ = this.menService.getProducts()
+  const loadProducts$ = this.loaderService.showLoader(products$);
+  this.womenProducts$ = loadProducts$.pipe(
+    map((res: any) => {
+      const productArray = res.products || [];
+      return productArray.filter(
+        (product: any) =>
+          product.category.name == "Women"
+        );
+    })
+  );
+}
+
+openProductDetails(product: any){
+  console.log("Product Details: ", product)
+  const dialogConfig = new MatDialogConfig()
+  dialogConfig.data = product
+  dialogConfig.width = '1024px'
+  dialogConfig.autoFocus = true
+  dialogConfig.disableClose = true
+  this.dialog.open(ProductDetailsComponent, dialogConfig)
+}
 
 }
